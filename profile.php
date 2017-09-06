e356200885f67efc1bfa083693e0f9938f106135
Anoop <anoop@inkers.in>
2017-09-06 20:55:37 +0530

    First Commit

---
+++ profile.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Eddey</title>
+    <meta name="keywords" content="" />
+	<meta name="description" content="" />
+    <!-- 
+    Powerful Template 
+    http://www.templatemo.com/tm-390-powerful
+    -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/templatemo_style.css" rel="stylesheet">
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="css/circle.css" rel="stylesheet">
+    <link href="css/jquery.bxslider.css" rel="stylesheet" />
+    <link rel="stylesheet" href="css/nivo-slider.css">
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,100,600' rel='stylesheet' type='text/css'>
+    <script src="js/modernizr.custom.js"></script>
+	  
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  <style>
+	  	.nav-pills>li.active>a, .nav-pills>li.active>a:hover, .nav-pills>li.active>a:focus {
+    	color: #fff;
+		background-color: #40c1ee;
+	    }  
+	  	img {
+		  width: 100%;
+			height: auto;
+	  	}
+  </style>
+	 <div class="mWrapper">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4 col-md-4">
+            <div class="logo">
+              <a href="#"><img src="images/logo.png" alt="Powerful Theme"></a>
+            </div>
+          </div>
+          <div class="col-sm-8 col-md-8">
+            <nav class="mainMenu">
+              <ul class="nav">
+                <li><a href="#templatemo_top">Home</a></li>
+                
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div> 
+	<div>
+		<div>
+		</div>
+		<div class="container">
+			<div>
+				<img src="images/logo.png" width="460" height="345">
+			</div>
+			<div class="row">
+				<div class="col-sm-2"></div>
+  				<div class="col-sm-8">
+					<ul class="nav nav-pills">
+						<li class="active"><a data-toggle="pill" href="#home">Home</a></li>
+						<li><a data-toggle="pill" href="#menu1">Info</a></li>
+						<li><a data-toggle="pill" href="#menu2">Contact Details</a></li>
+						<li><a data-toggle="pill" href="#menu3">Courses</a></li>
+						<li><a data-toggle="pill" href="#menu4">Hostel</a></li>
+						<li><a data-toggle="pill" href="#menu5">Bus Facilities</a></li>
+						<li><a data-toggle="pill" href="#menu6">Infrastructure</a></li>
+						<li><a data-toggle="pill" href="#menu7">Gallery</a></li>
+					</ul>
+				</div>
+				<div class="col-sm-2"></div>
+			</div>
+  		<div class="tab-content">
+			<div id="home" class="tab-pane fade in active">
+      		<h3>HOME</h3>
+      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</</p>
+      		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    	</div>
+		<div id="menu1" class="tab-pane fade">
+			<h3>Menu 1</h3>
+      		<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			</div>
+    	<div id="menu2" class="tab-pane fade">
+			<h3>Menu 2</h3>
+			<p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.</p>
+    	</div>
+    	<div id="menu3" class="tab-pane fade">
+      		<h3>Menu 3</h3>
+      		<p>Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+		</div>
+		</div>
+		</div>
+	</div>   
+	<div style="padding-bottom: 13px; background: rgba(0, 172, 233, 0.75);" class="text-center">
+        <div style="margin-top: 66px; padding-top: 15px;" class="templatemo_footer">Copyright © 2017 Eddey Powered By <a href="http://inkers.in" style="color: #ffffff; font-weight: bolder;">inkers</a></div>
+      </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <!-- <script src="https://code.jquery.com/jquery.js"></script> -->
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.cycle2.min.js"></script>
+    <script src="js/jquery.cycle2.carousel.min.js"></script>
+    <script src="js/jquery.nivo.slider.pack.js"></script>
+    <script>$.fn.cycle.defaults.autoSelector = '.slideshow';</script>
+    <script src="js/jquery.cookie.js"></script>
+    <script type="text/javascript">
+  </body>
+</html>
